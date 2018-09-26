8d07a6ffe4886ff343db7f04d0cf692881854272
Zoey Dao <yendao.zoey@gmail.com>
2018-09-27 01:26:33 +0700

    turtle 2 update

--- session2/homework/turtle2.py
+++ session2/homework/turtle2.py
@@ -1,12 +1,37 @@
 from turtle import *
-left(120)
-for _ in range(6):
-    forward(100)
-    right (60)
-right(10)
-for _ in range(5):
-    forward(100)
-    right(80)
 
-    
+length = 100
+num_sides = 3
+
+#triangle
+pencolor("blue")
+left(60)
+for _ in range(3):
+    forward(length)
+    right(360/num_sides)
+#square
+pencolor("red")
+left(30)
+num_sides = 4
+for _ in range(4): 
+    forward(length)
+    right(360/num_sides)
+#pentagon
+pencolor("blue")
+left(18)
+num_sides = 5
+for _ in range(5): 
+    forward(length)
+    right(360/num_sides)
+#hectagon
+pencolor("red")
+left(12)
+num_sides = 6
+for _ in range(6): 
+    forward(length)
+    right(360/num_sides)   
+
+right(120)
+fillcolor("red")
+
 mainloop()
