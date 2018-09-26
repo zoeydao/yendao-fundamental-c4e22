70edfec39043834c37fe674974ac0e991b97d1c5
Zoey Dao <yendao.zoey@gmail.com>
2018-09-27 01:28:46 +0700

    turtle 2 update

--- session2/homework/turtle2.py
+++ session2/homework/turtle2.py
@@ -6,30 +6,30 @@ num_sides = 3
 #triangle
 pencolor("blue")
 left(60)
-for _ in range(3):
+for _ in range(num_sides): 
     forward(length)
     right(360/num_sides)
 #square
 pencolor("red")
 left(30)
-num_sides = 4
-for _ in range(4): 
+num_sides += 1
+for _ in range(num_sides): 
     forward(length)
     right(360/num_sides)
 #pentagon
 pencolor("blue")
 left(18)
-num_sides = 5
-for _ in range(5): 
+num_sides += 1
+for _ in range(num_sides): 
     forward(length)
     right(360/num_sides)
 #hectagon
 pencolor("red")
 left(12)
-num_sides = 6
-for _ in range(6): 
+num_sides += 1
+for _ in range(num_sides): 
     forward(length)
-    right(360/num_sides)   
+    right(360/num_sides) 
 
 right(120)
 fillcolor("red")
